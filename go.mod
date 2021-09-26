07107c2fe574b8bacf15cff136946c7b8872bb45
Manolis Skouris <skourismanolis@gmail.com>
2021-09-26 13:48:49 +0300

    tidy code and deps, gitignore

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/skourismanolis/goload-manager
 
 go 1.17
 
-require github.com/gosuri/uiprogress v0.0.1
+require (
+	github.com/cavaliercoder/grab v2.0.0+incompatible
+	github.com/gosuri/uiprogress v0.0.1
+)
 
 require (
-	github.com/cavaliercoder/grab v2.0.0+incompatible // indirect
 	github.com/gosuri/uilive v0.0.4 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	// "strings"
 
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,17 +14,38 @@ import (
 	// "github.com/skourismanolis/goload-manager/test"
 )
 
-func initBar(resp *grab.Response) *uiprogress.Bar {
+func initBar(download *grab.Response) *uiprogress.Bar {
 	bar := uiprogress.AddBar(100) // Add a new bar
 	// optionally, append and prepend completion and elapsed time
 	bar.Empty = '_'
 	bar.Fill = '#'
 	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		return resp.Filename
+		return download.Filename
 	})
 	bar.AppendCompleted()
 	// bar.PrependElapsed()
 	return bar
+}
+
+func monitorDownload(download *grab.Response) {
+	bar := initBar(download)
+
+	fmt.Println("asd")
+	t := time.NewTicker(time.Millisecond * 50)
+	defer t.Stop()
+
+Loop:
+	for {
+		select {
+		case <-t.C:
+			fmt.Println(download.Progress())
+			bar.Set(int(download.Progress() * 100))
+		case <-download.Done:
+			// download is complete
+			break Loop
+		}
+	}
+
 }
 
 func init() {
@@ -41,28 +63,19 @@ func main() {
 		var line string
 		fmt.Printf("URL> ")
 		fmt.Scanln(&line)
+		if strings.ToLower(line) == "exit" {
+			break
+		}
 
-		req, _ := grab.NewRequest("", line)
-		download := client.Do(req)
+		req, _ := grab.NewRequest("./downloads", line)
 
 		wg.Add(1)
 		go func() {
+			download := client.Do(req)
 			defer wg.Done()
-			bar := initBar(download)
-			t := time.NewTicker(time.Millisecond * 500)
-			defer t.Stop()
-
-		Loop:
-			for {
-				select {
-				case <-t.C:
-					bar.Set(int(download.Progress() * 100))
-				case <-download.Done:
-					// download is complete
-					break Loop
-				}
-			}
+			monitorDownload(download)
 		}()
+
 	}
 
 	wg.Wait()
